dd8283cc9dfcebc1e540346dc9dc86799805169c
Utkarsh Sharma <utkarsh.sharma@cimpress.com>
2020-09-04 12:04:48 +0530

    testing save object

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,33 @@
-import React from "react"
+import React,{ useState, useEffect } from "react"
 import { Link } from "gatsby"
+import algoliasearch from 'algoliasearch';
 
 import { rhythm, scale } from "../utils/typography"
+
+const algoliaResource = {
+  appId:`X89O3S3OTK`,
+  apiKey:`109786d34bd9069f1dbda1e84e9ccd64`,
+  index:'sample_data'
+}
+
+const sampleDataToAdd = [
+   {objectID:"sampleObjId",productName:"jjhvidfjv",category:"nkdfjge",pid:'knfdlgn'},
+   {objectID:"sampleObjId2",productName:"jjhvidfjv",category:"nkdfjge",pid:'knfdlgn_2'},{objectID:"sampleObjId3",productName:"jjhvidfjv",category:"nkdfjge",pid:'knfdlgn_3'},
+   {objectID:"sampleObjId4",productName:"jjhvidfjv",category:"nkdfjge",pid:'knfdlgn_4'},
+   {objectID:"sampleObjId5",productName:"jjhvidfjv",category:"nkdfjge",pid:'knfdlgn_4'}
+]
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
+
+  useEffect(()=>{
+       const client = algoliasearch(algoliaResource.appId, algoliaResource.apiKey,{protocol: 'https:'});
+       const index =client.initIndex(algoliaResource.index);
+       if(index){
+         index.saveObjects(sampleDataToAdd)
+       }  
+  },[])
 
   if (location.pathname === rootPath) {
     header = (
@@ -47,6 +69,8 @@ const Layout = ({ location, title, children }) => {
       </h3>
     )
   }
+
+
   return (
     <div
       style={{
@@ -61,7 +85,7 @@ const Layout = ({ location, title, children }) => {
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <a href="https://www.gatsbyjs.org">Gatsby temp</a>
       </footer>
     </div>
   )
